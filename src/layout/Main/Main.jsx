fc0403626a2d71b5bfe65b52b6a277559f93adf6
MarzukaaZaki <zakimarzuka@gmail.com>
2023-11-09 00:58:43 +0600

    Added the qr scanning function and displayed content and thumbnail of qr code

--- src/layout/Main/Main.jsx
+++ src/layout/Main/Main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import './Main.css'
 
 const Main = () => {
     return (
-        <div>
+        <div className='container'>
             <Outlet/>
         </div>
     );
--- src/pages/QRScanner/QRScanner.jsx
+++ src/pages/QRScanner/QRScanner.jsx
@@ -1,9 +1,74 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import { toPng } from 'html-to-image';
+import { QRCodeSVG } from 'qrcode.react';
+import { QrReader } from 'react-qr-reader';
 import './QRScanner.css'
+
+
 const QRScanner = () => {
+    const [data, setData] = useState('');
+    const [qrCodeUrl, setQrCodeUrl] = useState('');
+    const qrCodeRef = useRef();
+
+    const handleQRScan = (qrInfo) => {
+        if (qrInfo) {
+            const qrText = qrInfo.text
+            setData(qrText); // capture content
+        }
+        setTimeout(() =>{
+            if(qrCodeRef.current){
+                toPng(qrCodeRef.current)
+                .then((dataUrl)=> {
+                    console.log(dataUrl);
+                    setQrCodeUrl(dataUrl);
+                })
+                .catch((error) =>{
+                    console.error('Failed to convert SVG to PNG', error);
+                })
+            }
+        }, 0)
+ 
+    }
+    const handleError = (error) => {
+        console.error(error);
+    };
+
     return (
-        <div>
-            This is QRScanner
+        <div style={{ width:'500px', textAlign:'center'}}>
+            <QrReader
+                constraints={{ facingMode: 'environment' }}
+                onResult={handleQRScan}
+                
+
+            />
+            
+            <div className='qr-reader-container' style={{ border: '2px solid black',  borderRadius:'10px', padding:'10px 25px'}}>
+                {
+                data ?     
+                <>
+                <p><strong>Content:</strong> {data}</p>
+                <div ref={qrCodeRef}>
+                <p><strong> Thumbnail: </strong></p>
+                <QRCodeSVG
+                id='qr-code-svg'
+                bgColor='#FFFFFF'
+                fgColor='#000000'
+                style={{width: 128}}
+                value={data}
+                /></div>
+                
+                </>
+                :
+                <p>Your QR Code info will be displayed here...</p>
+                
+                }
+                {/* <img src={qrCodeUrl}/> */}
+
+
+            
+            </div>
+
+
         </div>
     );
 };
